8784ef0ec4ce1c6abaf0fbc12b91cec2935a27e4
lenorastevens <tlbs6513@gmail.com>
2023-04-26 17:25:50 -0500

    prep 3 program work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess a number between 1 and 20.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 21);
+       
+        int guess = -1;
+        int numOfGuesses = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+            numOfGuesses += 1;
+
+        
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }    
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("You guessed it?");
+                Console.WriteLine($"It took {numOfGuesses} guesses to get the right answer.");
+            }
+            
+        }
+
+
+
+
     }
 }
