fd05914e88877c5cb9c18c78a61e83b9e1a76104
lenorastevens <tlbs6513@gmail.com>
2023-04-26 19:04:31 -0500

    prep 4 work done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int number = 0;
+        string userNumber = "";
+        int sum = 0;
+        int maxNum = 0;
+        float ave = 0;
+        int smallestPosNum = 99999;
+
+        while (userNumber != "0")
+        {
+            Console.Write("Enter number: ");
+            userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+            numbers.Add(number);
+
+            // Compute the sum, or total, of the numbers in the list.
+            sum += number;
+
+            // Find the maximum, or largest, number in the list.
+            
+            foreach (int integer in numbers)
+            {
+                if(number > maxNum)
+                {
+                    maxNum = number;   
+                }
+                if(number > 0 && number < smallestPosNum)
+                {
+                    smallestPosNum = number;
+                }
+            }
+        }
+        // Compute the average of the numbers in the list.
+            ave = ((float)sum) / ((numbers.Count) -1);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {ave}");
+        Console.WriteLine($"The largest number is: {maxNum}");
+        Console.WriteLine($"The smallest positive number is: {smallestPosNum}");
+        Console.WriteLine("The sorted list is: ");
+
+        numbers.Sort();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int sortedNum = numbers[i];
+            Console.WriteLine(sortedNum);
+        }
+            
+
     }
 }
