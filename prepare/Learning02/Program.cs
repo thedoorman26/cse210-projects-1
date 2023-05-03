d827ef448c136582b572e5e597c9b1b8b816cc96
lenorastevens <tlbs6513@gmail.com>
2023-05-02 21:35:25 -0500

    learn02 work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+            job1._jobTitle = "Software Engineer";
+            job1._company = "Qualcom";
+            job1._startYear = 2011;
+            job1._endYear = 2015;
+
+        
+        Job job2 = new Job();
+            job2._jobTitle = "Quality Manager";
+            job2._company = "Facebook";
+            job2._startYear = 2015;
+            job2._endYear = 2019;
+
+
+        Resume resume1 = new Resume();
+            resume1._name = "Lenora Stevens";
+            resume1._jobs.Add(job1);
+            resume1._jobs.Add(job2);
+
+        resume1.showResumeDetails();
+
+        
+
     }
 }
